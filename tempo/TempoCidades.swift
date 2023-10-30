bb8d926a910cdd7ce4eab85e68a0679fbb3e3faf
I54guerra <isadora.guerra@edu.pucrs.br>
2023-10-30 10:46:58 -0300

    cidades

---
+++ tempo/TempoCidades.swift
@@ -0,0 +1,18 @@
+//
+//  TempoCidades.swift
+//  tempo
+//
+//  Created by Isadora Guerra on 30/10/23.
+//
+
+import SwiftUI
+
+struct TempoCidades: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TempoCidades()
+}
