7d268075e4c07f2184eb1e175d42b00bc5b4e05e
I54guerra <isadora.guerra@edu.pucrs.br>
2023-10-30 10:30:37 -0300

    TempoDia

---
+++ tempo/TempoDia.swift
@@ -0,0 +1,18 @@
+//
+//  TempoDia.swift
+//  tempo
+//
+//  Created by Isadora Guerra on 30/10/23.
+//
+
+import SwiftUI
+
+struct TempoDia: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TempoDia()
+}
