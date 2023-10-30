aa321ab09266a90b5f9aaf95bae2a6ef510c3acb
I54guerra <isadora.guerra@edu.pucrs.br>
2023-10-30 10:16:59 -0300

    Adicionando projeto

---
+++ tempo/tempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempoApp.swift
+//  tempo
+//
+//  Created by Isadora Guerra on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct tempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
